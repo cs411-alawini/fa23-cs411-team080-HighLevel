4fac1b920225123e9651223151534e637b1cbd0b
xinyu chen <xinyuc10@illinois.edu>
2023-11-29 00:20:56 -0600

    searchg in flight and trip

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 
-from app.dbop import delete_user, flight_search,insert_user, get_user, update_user
+from app.dbop import delete_user, detail_search, flight_search,insert_user, get_user, update_user
 
 app = Flask( __name__)
 
@@ -45,7 +45,57 @@ def search_flight():
     TAIL_NUMBER = request.json('TAIL_NUMBER')
     information = flight_search(FLIGHT_ID,FLIGHT_NUMBER,AIRLINE_IATA,TAIL_NUMBER)
     if information is not None:
-        return jsonify(information)
+        return jsonify(information),200
+    else:
+        return "Flight not found", 404
+    
+@app.route('/search_detail',methods = ['GET'])
+def search_flight():
+    FLIGHT_ID = request.json('FLIGHT_ID')
+    FLIGHT_NUMBER = request.json('FLIGHT_NUMBER')
+    AIRLINE_IATA = request.json('AIRLINE_IATA')
+    TAIL_NUMBER = request.json('TAIL_NUMBER')
+    
+    YEAR = request.json('YEAR')
+    MONTH = request.json('MONTH')
+    DAY = request.json('DAY')
+    DAY_OF_WEEK = request.json('DAY_OF_WEEK')
+    SCHEDULED_DEPARTURE = request.json('SCHEDULED_DEPARTURE')
+    DEPARTURE_TIME = request.json('DEPARTURE_TIME')
+    DEPARTURE_DELAY = request.json('DEPARTURE_DELAY')
+    TAXI_OUT = request.json('TAXI_OUT')
+    WHEELS_OFF = request.json('WHEELS_OFF')
+    SCHEDULED_TIME = request.json('SCHEDULED_TIME')
+    ELAPSED_TIME = request.json('ELAPSED_TIME')
+    AIR_TIME = request.json('AIR_TIME')
+    DISTANCE = request.json('DISTANCE')
+    WHEELS_ON = request.json('WHEELS_ON')
+    TAXI_IN = request.json('TAXI_IN')
+    SCEHDULED_ARRIVAL = request.json('SCEHDULED_ARRIVAL')
+    ARRIVAL_TIME = request.json('ARRIVAL_TIME')
+    ARRIVAL_DELAY = request.json('ARRIVAL_DELAY')
+    DIVERTED = request.json('DIVERTED')
+    CANCELED = request.json('CANCELED')
+    CANCELLATION_REASON = request.json('CANCELLATION_REASON')
+    AIR_SYSTEM_DELAY = request.json('AIR_SYSTEM_DELAY')
+    SECURITY_DELAY = request.json('SECURITY_DELAY')
+    AIRLINE_DELAY = request.json('AIRLINE_DELAY')
+    LATE_AIRCRAFT_DELAY = request.json('LATE_AIRCRAFT_DELAY')
+    WEATHER_DELAY = request.json('WEATHER_DELAY')
+    ORIGIN_AIRPORT = request.json('ORIGIN_AIRPORT')
+    TAIL_NUMBER = request.json('TAIL_NUMBER')
+    SCHEDULED_ARRIVAL = request.json('SCHEDULED_ARRIVAL')
+    
+    information = detail_search(FLIGHT_ID, FLIGHT_NUMBER, YEAR, MONTH, DAY, 
+                  DAY_OF_WEEK, SCHEDULED_DEPARTURE, DEPARTURE_TIME, 
+                  DEPARTURE_DELAY, TAXI_OUT, WHEELS_OFF, SCHEDULED_TIME, 
+                  ELAPSED_TIME, AIR_TIME, DISTANCE, WHEELS_ON, TAXI_IN, 
+                  SCHEDULED_ARRIVAL, ARRIVAL_TIME, ARRIVAL_DELAY, DIVERTED, 
+                  CANCELED, CANCELLATION_REASON, AIR_SYSTEM_DELAY, 
+                  SECURITY_DELAY, AIRLINE_DELAY, LATE_AIRCRAFT_DELAY, 
+                  WEATHER_DELAY, ORIGIN_AIRPORT, TAIL_NUMBER)
+    if information is not None:
+        return jsonify(information),200
     else:
         return "Flight not found", 404
     
