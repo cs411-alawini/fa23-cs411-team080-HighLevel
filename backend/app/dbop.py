6bb8e1eaca48656fc1d4d41dc47ba6e81db4230a
capTony02Zheng <363411246@qq.com>
2023-12-02 16:52:30 -0600

    connect isuuse fixed

--- backend/app/dbop.py
+++ backend/app/dbop.py
@@ -171,7 +171,7 @@ def update_booking(BOOKING_ID,USER_ID, FLIGHT_ID):
 def search_airport(ORIGIN_AIRPORT,DESTINATION_AIRPORT):
     db = get_db()
     cursor = db.cursor()
-    query = "SELECT * FROM ToAndFrom JOIN Trip on Trip.FLIGHT_ID = ToAndFrom.FLIGHT_ID WHERE ORIGIN_AIRPORT = '{}' AND DESTINATION_AIRPORT = '{}'".format(ORIGIN_AIRPORT,DESTINATION_AIRPORT)
+    query = "SELECT * FROM ToAndFrom JOIN Trip on Trip.FLIGHT_ID = ToAndFrom.FLIGHT_ID WHERE ToAndFrom.ORIGIN_AIRPORT = '{}' AND ToAndFrom.DESTINATION_AIRPORT = '{}'".format(ORIGIN_AIRPORT,DESTINATION_AIRPORT)
     cursor.execute(query),
     airport = cursor.fetchall()
     cursor.close()
